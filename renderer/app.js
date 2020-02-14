cbb66d6116b2a39b0bea2cd50db0509f49332b6f
CaseyInHaengsin <casey.rowley89@gmail.com>
2020-02-14 07:57:35 -0700

    I am now creating item elements and adding them to the app.

--- renderer/app.js
+++ renderer/app.js
@@ -1,7 +1,7 @@
 
 // Modules
 const {ipcRenderer} = require('electron')
-
+const items = require('./items');
 // Dom Nodes
 let showModal = document.getElementById('show-modal'),
     closeModal = document.getElementById('close-modal'),
@@ -53,10 +53,8 @@ addItem.addEventListener('click', e => {
 
 // Listen for new item from main process
 ipcRenderer.on('new-item-success', (e, newItem) => {
-  console.log('made it back');
-  console.log(newItem)
-
   // Enable buttons
+  items.addItem(newItem);
   toggleModalButtons()
 
   // Hide modal and clear value
