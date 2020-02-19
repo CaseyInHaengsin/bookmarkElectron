51fd220b6ec673c7d31699404b9ccb7c1b0fc0e8
CaseyInHaengsin <casey.rowley89@gmail.com>
2020-02-19 06:21:19 -0700

    adding a custom menu and linking out to github to learn more ;).

---
+++ renderer/menu.js
@@ -0,0 +1,48 @@
+
+
+const { remote, shell } = require('electron');
+
+const template = [
+    {
+        label: 'Items', 
+        submenu: []
+    },
+    {
+        role: 'editMenu'
+    },
+    {
+        role: 'windowMenu'
+    },
+    {
+        role: 'help', 
+        submenu: [
+            {
+                label: 'Learn more',
+                click: () => { shell.openExternal('https://github.com/CaseyInHaengsin') }
+            }
+        ]
+    }
+]
+
+if (process.platform == 'darwin'){
+    template.unshift({
+        label: remote.app.getName(),
+        submenu: [
+            { role: 'about' },
+            { type: 'separator' },
+            { role: 'services' },
+            { role: 'services' },
+            { role: 'hide' },
+            { role: 'hideothers' },
+            { role: 'unhide' },
+            { role: 'services' },
+            { role: 'quit' },
+
+        ]
+    })
+}
+
+const menu = remote.Menu.buildFromTemplate(template);
+
+
+remote.Menu.setApplicationMenu(menu);
