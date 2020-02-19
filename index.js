8d7e9cc12be73661e5c7a67dac0fd082fe00b9a2
CaseyInHaengsin <casey.rowley89@gmail.com>
2020-02-19 06:53:05 -0700

    Starting to build dmg to distribute.

--- index.js
+++ index.js
@@ -39,8 +39,8 @@ function createWindow () {
   // Manage new window state
   state.manage(mainWindow)
 
-  // Open DevTools - Remove for PRODUCTION!
-  mainWindow.webContents.openDevTools();
+  
+  
 
   // Listen for window being closed
   mainWindow.on('closed',  () => {
